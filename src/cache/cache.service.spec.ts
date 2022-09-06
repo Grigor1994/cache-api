52f1f2d054ec49e3caf8d922f20ee693ae22a0c6
grigor1994 <grigor.avetisyan@hotmail.com>
2022-09-06 17:30:40 +0400

    tests fix

--- src/cache/cache.service.spec.ts
+++ src/cache/cache.service.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { CacheService } from './cache.service';
 import { getModelToken } from '@nestjs/mongoose';
 import { Cache } from './schemas/cache.schema';
+import { Model } from 'mongoose';
 
 const mockCache = {
   key: '5516f8f3-956e-4f88-9f53-4367855844525154',
@@ -11,39 +12,62 @@ const mockCache = {
 
 describe('CacheService', () => {
   let service: CacheService;
+  let model: Model<Cache>;
+
+  const mockKey = {
+    key: '5516f8f3-956e-4f88-9f53-4367855844525152',
+  };
 
   const keysArray = [
     {
       key: '5516f8f3-956e-4f88-9f53-4367855844525152',
-      value: 'WkKUjj7kSHf62tC7ZJtIoUCzd7Cy0j63kBYdoWOvSnmDf2VVbq',
-      expireAt: 1462326684803,
     },
     {
       key: '5516f8f3-956e-4f88-9f53-43678558445251',
-      value: 'WkKUjj7kSHf62tC7ZJtIoUCzd7Cy0j63kBYdoWOvSnmDf2VVbq',
-      expireAt: 1656326684803,
     },
     {
       key: '4f88-956e-4f88-9f53-5516f8f35516f8f3',
-      value: 'WkKUjj7kSHf62tC7ZJtIoUCzd7Cy0j63kBOvSnmDf2VVbq',
-      expireAt: 14526684803,
     },
     {
       key: '5516f8f3-956e-4f88-9f53-4367855844525152',
-      value: 'WkKUjj7kSHf62tC7ZJtIoUCzd7C4367855844525152oWOvSnmDf2VVbq',
-      expireAt: 1662326684803,
     },
   ];
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         CacheService,
-        { provide: getModelToken(Cache.name), useValue: jest.fn() },
+        {
+          provide: getModelToken(Cache.name),
+          useValue: {
+            new: jest.fn().mockResolvedValue(mockCache),
+            constructor: jest.fn().mockResolvedValue(mockCache),
+            find: jest.fn(),
+            create: jest.fn(),
+            select: jest.fn(),
+            count: jest.fn(),
+            findOneAndUpdate: jest.fn(),
+          },
+        },
       ],
     }).compile();
 
     service = module.get<CacheService>(CacheService);
-    jest.clearAllMocks();
+    model = module.get<Model<Cache>>(getModelToken('Cache'));
+    model.deleteOne = jest.fn().mockResolvedValue(() => Promise.resolve(true));
+    model.deleteMany = jest.fn().mockResolvedValue(() => Promise.resolve(true));
+    model.create = jest.fn().mockResolvedValue(
+      (): Promise<Cache> =>
+        Promise.resolve({
+          ...mockCache,
+        }),
+    );
+    model.findOne = jest.fn().mockResolvedValue(
+      (): Promise<Cache> =>
+        Promise.resolve({
+          ...mockCache,
+        }),
+    );
+    model.count = jest.fn().mockResolvedValue(() => Promise.resolve(0));
   });
 
   it('should be defined', () => {
@@ -51,36 +75,31 @@ describe('CacheService', () => {
   });
 
   it('should return all cache keys', async () => {
-    const getAllKeys = jest.spyOn(service, 'getAllKeys');
-    getAllKeys.mockImplementationOnce(
-      (): Promise<Cache[]> => Promise.resolve([...keysArray]),
-    );
+    jest.spyOn(model, 'find').mockReturnValue({
+      select: jest.fn().mockResolvedValueOnce(keysArray),
+    } as any);
     const keys = await service.getAllKeys();
     expect(keys).toEqual(keysArray);
   });
-  it('should create or update data by key', async () => {
+
+  it('should delete data from cache by key', async () => {
     const key = '5516f8f3-956e-4f88-9f53-4367855844525154';
-    jest.spyOn(service, 'createOrUpdateByKey').mockImplementationOnce(() =>
-      Promise.resolve({
-        key: '5516f8f3-956e-4f88-9f53-4367855844525154',
-        value: 'WkKUjj7kSHf62tC7ZJtIoUCzd7Cy0j63kBYdoWOvSnmDf2VVbq',
-        expireAt: 1662326684803,
-      }),
-    );
-    const newCache = await service.createOrUpdateByKey(key, true);
-    expect(newCache).toEqual(mockCache);
+    await service.removeByKey(key);
+    expect(model.deleteOne).toHaveBeenCalledTimes(1);
   });
 
-  it('should return data by key', async () => {
+  it('should delete all stored data from cache', async () => {
+    await service.removeAll();
+    expect(model.deleteMany).toHaveBeenCalledTimes(1);
+  });
+
+  it('should get data by key', async () => {
     const key = '5516f8f3-956e-4f88-9f53-4367855844525154';
-    jest.spyOn(service, 'createOrUpdateByKey').mockImplementationOnce(() =>
-      Promise.resolve({
-        key: '5516f8f3-956e-4f88-9f53-4367855844525154',
-        value: 'WkKUjj7kSHf62tC7ZJtIoUCzd7Cy0j63kBYdoWOvSnmDf2VVbq',
-        expireAt: 1662326684803,
-      }),
-    );
-    const newCache = await service.createOrUpdateByKey(key, false);
-    expect(newCache).toEqual(mockCache);
+    jest.spyOn(model, 'findOneAndUpdate').mockReturnValue({
+      select: jest.fn().mockResolvedValueOnce(mockKey),
+    } as any);
+    const result = await service.createOrUpdateByKey(key);
+    expect(result).toEqual(mockKey);
+    expect(model.findOne).toHaveBeenCalledTimes(1);
   });
 });
